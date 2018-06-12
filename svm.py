a01f120a905b6c94e2380942164c7dce28b9a0fa
Łukasz <lukasz.szymczyk.94@gmail.com>
2018-06-13 00:01:20 +0200

    SVM

--- svm.py
+++ svm.py
@@ -7,74 +7,69 @@ from keras.layers import Activation, Dropout, Flatten, Dense, Dropout
 from keras.optimizers import RMSprop
 from keras.callbacks import ModelCheckpoint, EarlyStopping, ReduceLROnPlateau
 from keras import regularizers
+from sklearn import preprocessing
 import os #foldery i pliki
 import re #regular expressions
-import cv2 #opencv
-import random # random
-import time # czas
-import pickle # serializacja obiektów do pliku
 import numpy as np #biblioteka numeryczna, tablice wielowymiarowe i utils
-import matplotlib.pyplot as plt #rysowanie wykresów
-from sklearn.cluster import KMeans # algorytm k średnich
 from sklearn import datasets, svm, metrics
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from PIL import Image
+from keras.models import Model
 
 root_dir = os.path.join('D:\\', 'DYSK', 'SNR') # ścieżka do katalogu głównego gdzie opisane są boundingbox. Należy zmienić w zależności od środowiska uruchomienia.
 train_img_dir = os.path.join(root_dir, 'train') # ścieżka do katalogu głównej z folderami ptaków zbioru trenującego
 test_img_dir = os.path.join(root_dir, 'test') # ścieżka do katalogu głównej z folderami ptaków zbioru testującego
 network_input_size = 192
-batch_size = 1
-num_classes = 50
-conv_regularizer = regularizers.l1(0.01)
-conv_file_name = os.path.join(root_dir, "conv_final.best.hdf5") # plik w którym serializowane są wagi po procesie uczenia
+conv_file_name = os.path.join(root_dir, "conv_final_2.best.43-0.3800.hdf5") # plik w którym serializowane są wagi po procesie uczenia
 history_file_name = os.path.join(root_dir, "conv_final.history.p") # plik w którym serializowana jest historia nauki
 
-# this is the augmentation configuration we will use for training
-train_datagen = ImageDataGenerator(rescale=1./255)
-
-# this is the augmentation configuration we will use for testing:
-# only rescaling
-test_datagen = ImageDataGenerator(rescale=1./255)
-
-# this is a generator that will read pictures found in
-# subfolers of 'data/train', and indefinitely generate
-# batches of augmented image data
-train_generator = train_datagen.flow_from_directory(
-        train_img_dir,  # this is the target directory
-        target_size=(network_input_size, network_input_size),  # all images will be resized to this size
-        batch_size=batch_size)  # since we use binary_crossentropy loss, we need binary labels
-
-# this is a similar generator, for validation data
-validation_generator = test_datagen.flow_from_directory(
-        test_img_dir,
-        target_size=(network_input_size, network_input_size),
-        batch_size=batch_size)
-
-model = Sequential()
-model.add(Conv2D(32, (3, 3), input_shape=(network_input_size, network_input_size, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(32, (1, 1), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Conv2D(32, (3, 3), activation='relu'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Flatten())
-model.add(Dense(50, activation='relu')) ##, kernel_regularizer=regularizers.l2(0.01)
-model.add(Dense(num_classes, activation='softmax'))
-model.summary(line_length = 70)
-
-model.load_weights(conv_file_name, by_name=False)
-model.layers.pop()
-model.summary(line_length = 70)
-model.compile(loss='categorical_crossentropy',
-            optimizer=RMSprop(),
-            metrics=['categorical_accuracy', 'top_k_categorical_accuracy'])
+model = keras.models.load_model(conv_file_name)
+layer_name = 'dense_1'
+to_svm_layer_model = Model(inputs=model.input, outputs=model.get_layer(layer_name).output)
 
 
-train_images_features = model.predict_generator(train_generator, steps=len(train_generator.filenames))
-test_images_features = model.predict_generator(validation_generator, steps=len(validation_generator.filenames))
+def load_data(model, img_dir, img_size):
+	X_set = []
+	Y_set = []
+	for root, subdirs, files in os.walk(img_dir):	
+		for directory in subdirs:
+			class_name = directory
+			for root, subdirs, files in os.walk(os.path.join(img_dir, directory)):
+				for file in files:
+					file_path = os.path.join(img_dir, directory, file)
+					img = Image.open(file_path)
+					img = img.resize((img_size,img_size))
+					rgb = Image.new("RGB", img.size, (255, 255, 255))
+					rgb.paste(img)
+					
+					arr = img_to_array(rgb)
+					image_features =  model.predict(np.array([arr]))
+					image_features = image_features.flatten()
+					image_features = np.array(image_features.tolist())
+					X_set.append(image_features)
+					Y_set.append(directory)
+	Y_set = np.array(Y_set)
+	Y_set = Y_set.astype(np.int32)
+	X_set = np.array(X_set)
+	X_set = preprocessing.scale(X_set)
+	return	X_set, Y_set
 
-classifier = svm.SVC(C=1, gamma=0.001)
-classifier.fit(train_images_features, train_generator.classes)
-predicted = classifier.predict(test_images_features)
+X_train, y_train = load_data(to_svm_layer_model, train_img_dir, 192)
+X_test, y_test = load_data(to_svm_layer_model, test_img_dir, 192)
 
+C_parameters =[0.1,0.2,0.5,1,2,5,10,20,50,100,200,500]
+print('SVM - polynominal kernel - tests')
+for x in C_parameters:
+    print(x)
+    clf = svm.SVC(kernel='poly', degree=2, C=x)
+    clf.fit(X_train, y_train)
+    print("Train: {0:.4f}".format(clf.score(X_train, y_train)))
+    print("Test: {0:.4f}".format(clf.score(X_test, y_test)))
+	
+print('SVM - rbf kernel - tests')
+for x in C_parameters:
+    print(x)
+    clf = svm.SVC(C=x)
+    clf.fit(X_train, y_train)
+    print("Train: {0:.4f}".format(clf.score(X_train, y_train)))
+    print("Test: {0:.4f}".format(clf.score(X_test, y_test)))
